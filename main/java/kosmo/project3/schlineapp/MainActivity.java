05599546914a8bdef5c24c942aa5e857bab69e1f
heoseo <hsyoon1799@naver.com>
2021-02-08 10:47:06 +0900

    1st commit

---
+++ main/java/kosmo/project3/schlineapp/MainActivity.java
@@ -0,0 +1,17 @@
+package kosmo.project3.schlineapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+        // gjtjdbs 브랜치 테스트2
+        // heoseo 브랜치 테스트2
+    }
+}
