3315b44d296e5d6b129e175852c76fb667f3c8f9
= <hsyoon1799@naver.com>
2021-02-10 12:49:55 +0900

    로그인 페이지 구현

--- main/java/kosmo/project3/schlineapp/MainActivity.java
+++ main/java/kosmo/project3/schlineapp/MainActivity.java
@@ -1,9 +1,35 @@
 package kosmo.project3.schlineapp;
 
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.content.res.Resources;
+import android.os.AsyncTask;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import org.json.JSONObject;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.net.HttpURLConnection;
+import java.net.Inet4Address;
+import java.net.InetAddress;
+import java.net.NetworkInterface;
+import java.net.SocketException;
+import java.net.URL;
+import java.security.Security;
+import java.util.Enumeration;
 
 public class MainActivity extends AppCompatActivity {
+
+    String TAG = "MainActivity";
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -11,7 +37,6 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
 
-        // gjtjdbs 브랜치 테스트2
-        // heoseo 브랜치 테스트2
     }
+
 }
---
+++ main/java/kosmo/project3/schlineapp/StaticInfo.java
@@ -0,0 +1,8 @@
+package kosmo.project3.schlineapp;
+
+import android.content.res.Resources;
+
+public class StaticInfo {
+    // 자기 ip주소랑 포트번호 쓰기.
+    public static String my_ip = "192.168.0.9:9999";
+}
