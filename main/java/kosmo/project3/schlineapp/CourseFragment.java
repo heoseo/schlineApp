5f8a736a9b36f43e34848e3471f16fab686d2c35
daeunk06 <daeunk06@gmail.com>
2021-02-10 16:39:18 +0900

    조금

---
+++ main/java/kosmo/project3/schlineapp/CourseFragment.java
@@ -0,0 +1,10 @@
+package kosmo.project3.schlineapp;
+
+import android.support.v4.app.Fragment;
+
+public class CourseFragment  extends Fragment {
+public static final String TAG = "schline";
+
+
+
+}
