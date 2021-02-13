49bec578b1f7c12872778006c59ceec4c3847b69
= <hsyoon1799@naver.com>
2021-02-13 16:42:38 +0900

    heoseo

--- main/java/kosmo/project3/schlineapp/StaticInfo.java
+++ main/java/kosmo/project3/schlineapp/StaticInfo.java
@@ -4,5 +4,6 @@ import android.content.res.Resources;
 
 public class StaticInfo {
     // 자신의 "ip주소:포트번호" 쓰기.
-    public static String my_ip = "192.168.219.101:8080";
+    // 서윤 -> 172.30.1.1:9999
+    public static String my_ip = "172.30.1.1:9999";
 }
