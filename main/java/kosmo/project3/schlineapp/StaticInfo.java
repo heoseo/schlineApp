aa59d5d603c6bfc35729b3b19e896665c7ee09ae
= <hsyoon1799@naver.com>
2021-02-17 17:38:22 +0900

    manifest.xml

--- main/java/kosmo/project3/schlineapp/StaticInfo.java
+++ main/java/kosmo/project3/schlineapp/StaticInfo.java
@@ -6,7 +6,7 @@ public class StaticInfo {
 
 
     // 서윤 -> 172.30.1.1:9999 ///   192.168.0.9:9999
-    public static String my_ip = "192.168.219.12:9999";
+    public static String my_ip = "192.168.219.125:9999";
 
 
 
