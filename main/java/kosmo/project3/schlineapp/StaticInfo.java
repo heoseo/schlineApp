dad5bf49df6f20d32a1dac3966951a78dd2fbfa8
laveberry <berry@DESKTOP-056VMGN>
2021-02-22 20:35:03 +0900

    캘린더

--- main/java/kosmo/project3/schlineapp/StaticInfo.java
+++ main/java/kosmo/project3/schlineapp/StaticInfo.java
@@ -9,9 +9,9 @@ public class StaticInfo {
     //public static String my_ip = "192.168.0.24:9999";//성준
     //public static String my_ip = "192.168.219.120:9999";//성준학원
     //public static String my_ip = "192.168.0.9:9999";//다은
-    //public static String my_ip = "192.168.25.47:9999";//다혜
+    public static String my_ip = "192.168.25.47:9999";//다혜
     //public static String my_ip = "172.30.1.1:9999"; //주희
-    public static String my_ip = "192.168.0.155:9999"; //주희학원
+    //public static String my_ip = "192.168.0.155:9999"; //주희학원
     //public static String my_ip = "221.154.60.184:8080";
     //public static String my_ip = "192.168.0.24:9999";
 
